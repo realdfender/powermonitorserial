ffbd6fc87ac69af27ccfc6963d30ef80b50fd940
realdfender <43970103+realdfender@users.noreply.github.com>
2025-05-12 20:50:27 +0200

    Update powermonitorserial.cpp

--- components/powermonitorserial/powermonitorserial.cpp
+++ components/powermonitorserial/powermonitorserial.cpp
@@ -27,7 +27,14 @@ void PowerMonitorSerial::parse_values_() {
   while (epos != std::string::npos) {
     epos = s.find(' ', spos);
     int len = (epos == std::string::npos ? s.size() - spos : epos - spos);
-    values.push_back(parse_number<float>(s.substr(spos, len)).value_or(NAN));
+    float val = parse_number<float>(s.substr(spos, len)).value_or(NAN);
+    
+    // Apply the filter: report 0 if value < 105
+    if (!std::isnan(val) && val < 105.0f) {
+      val = 0.0f;
+    }
+
+    values.push_back(val);
     if (epos != std::string::npos)
       spos = epos + 1;
   }
